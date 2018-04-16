58738ae0aae7e15cbe63a7a98d15ee072da41b7f
aijams <aija1100@gmail.com>
2018-04-15 21:12:21 -0400

    created simulation class and setp function

--- src/main.py
+++ src/main.py
@@ -1,22 +1,41 @@
 import numpy as np
 
 class Environment:
-    def __init__(dims):
+    def __init__(self, dims, n_ants):
         # create the array of ants, containing their intensive properties such as
         # energy level and internal state variables
+        self.ants = np.zeros(n_ants, dtype=[('pos_x', np.int32), ('pos_y', np.int32),
+                                ('energy', np.float64),('state', np.int64)])
         # create the world and set its spatial variables
         self.world = np.zeros(dims, dtype=[('pheromone', np.float64),('food', np.float64)])
 
-    def step():
-        pass
+        # setup the world
+        self.ants[:]['pos_x'] = np.random.randint(0, high=dims[0], size=(n_ants,))
+        self.ants[:]['pos_y'] = np.random.randint(0, high=dims[0], size=(n_ants,))
+
+    def step(self):
+        pos_x = self.ants[:]['pos_x']
+        pos_y = self.ants[:]['pos_y']
+        energies = self.ants[:]['energy']
+        states = self.ants[:]['state']
+
+        pheromone_under_ants = self.world[pos_x, pos_y]['pheromone']
+        food_under_ants = self.world[pos_x, pos_y]['food']
+
+        # perform ant AI operations
+
+
 
 
 
 def main():
-    size = 500
+    size = 5
     dims = (size, size)
-    env = Environment(dims)
-
+    n_ants = 200
+    env = Environment(dims, n_ants)
+    n_steps = 1
+    for i in range(n_steps):
+        env.step()
 
 if __name__ == '__main__':
     main()
